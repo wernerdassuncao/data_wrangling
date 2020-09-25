80edba6d029af6ae7a495fc5908606481edc12cf
Werner Dassuncao <55885116+wernerdassuncao@users.noreply.github.com>
2020-09-24 21:03:00 -0700

    updated spread.R code

--- spread.R
+++ spread.R
@@ -4,6 +4,10 @@ path <- system.file("extdata", package="dslabs")
 filename <- file.path(path, "fertility-two-countries-example.csv")
 wide_data <- read_csv(filename)
 
+# SPREAD function
+# 1st argument: which variable will be used as the column name
+# 2nd argument: which variable to use to fill out the cells
+
 #previous code from gather.R 
 new_tidy_data <- wide_data %>%
   gather(year, fertility, -country, convert = TRUE)
